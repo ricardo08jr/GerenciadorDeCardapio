5d54d4f8f5e26e99cb2e300e6797e8c85bda6df0
Ricardo Junior <ricardo08jr@hotmail.com>
2026-03-15 18:39:23 -0300

    feat: add Entity Cardapio

--- gerenciador-cardapio-spring/src/main/java/Domain/Entities/Cardapio.java
+++ gerenciador-cardapio-spring/src/main/java/Domain/Entities/Cardapio.java
@@ -1,4 +1,23 @@
 package Domain.Entities;
+import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
+
+@Entity
+@Table(
+        name = "cardapio",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"data", "prato_id"})
+        }
+)
 public class Cardapio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private LocalDateTime data;
+
+    @ManyToOne
+    @JoinColumn(name = "prato_id")
+    private Prato prato;
 }
