f8b6ea8a8f1d40cdacd2ce491b21910807ee01aa
Ricardo Junior <ricardo08jr@hotmail.com>
2026-03-15 17:04:32 -0300

    feat: add starterProject

---
+++ gerenciador-cardapio-spring/src/main/java/com/gerenciador/cardapio/gerenciador_cardapio_spring/GerenciadorCardapioSpringApplication.java
@@ -0,0 +1,13 @@
+package com.gerenciador.cardapio.gerenciador_cardapio_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorCardapioSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorCardapioSpringApplication.class, args);
+	}
+
+}
---
+++ gerenciador-cardapio-spring/src/test/java/com/gerenciador/cardapio/gerenciador_cardapio_spring/GerenciadorCardapioSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciador.cardapio.gerenciador_cardapio_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorCardapioSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
