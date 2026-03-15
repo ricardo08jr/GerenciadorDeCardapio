53cf200519addb63d3860254845ffe9cf9631965
Ricardo Junior <ricardo08jr@hotmail.com>
2026-03-15 18:38:54 -0300

    feat: add Entity Prato

--- gerenciador-cardapio-spring/src/main/java/Domain/Entities/Prato.java
+++ gerenciador-cardapio-spring/src/main/java/Domain/Entities/Prato.java
@@ -1,4 +1,25 @@
 package Domain.Entities;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Prato {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String detalhe;
+
+    @ManyToOne
+    @JoinColumn(name = "proteina_id")
+    private Proteina proteina;
+
+    @ManyToOne
+    @JoinColumn(name="acompanhamento_id")
+    private Acompanhamento acompanhamento;
+
+    @ManyToOne
+    @JoinColumn(name = "salada_id")
+    private Salada salada;
+
 }
