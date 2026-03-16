ec633be1a379271758c26702e8de8c720e6165ec
Ricardo Junior <ricardo08jr@hotmail.com>
2026-03-15 21:23:11 -0300

    feat: create endpoint get of acompanhamento

--- gerenciador-cardapio-spring/src/main/java/com/gerenciador/cardapio/gerenciador_cardapio_spring/Api/Controllers/AcompanhamentoController.java
+++ gerenciador-cardapio-spring/src/main/java/com/gerenciador/cardapio/gerenciador_cardapio_spring/Api/Controllers/AcompanhamentoController.java
@@ -1,4 +1,25 @@
 package com.gerenciador.cardapio.gerenciador_cardapio_spring.Api.Controllers;
 
+import com.gerenciador.cardapio.gerenciador_cardapio_spring.Entities.Acompanhamento;
+import com.gerenciador.cardapio.gerenciador_cardapio_spring.Infrastructure.Repository.AcompanhamentoRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController// ela compões mais dois decorators
+// @Controller
+// @ResponseBody
+@RequestMapping("/acompanhamento")
 public class AcompanhamentoController {
+    private final AcompanhamentoRepository _acompanhamentoRepository;
+
+    public AcompanhamentoController(AcompanhamentoRepository acompanhamentoRepository){
+        this._acompanhamentoRepository = acompanhamentoRepository;
+    }
+    @GetMapping
+    public List<Acompanhamento> get(){
+        return _acompanhamentoRepository.findAll();
+    }
 }
--- gerenciador-cardapio-spring/src/main/java/com/gerenciador/cardapio/gerenciador_cardapio_spring/GerenciadorCardapioSpringApplication.java
+++ gerenciador-cardapio-spring/src/main/java/com/gerenciador/cardapio/gerenciador_cardapio_spring/GerenciadorCardapioSpringApplication.java
@@ -3,7 +3,10 @@ package com.gerenciador.cardapio.gerenciador_cardapio_spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // Porta de Entrada do Spring Boot composto por 3 outros
+// @Configuration  define que as classes definem bean
+// @EnableAutoCOnfiguration
+// ComponentScan -> Permitir que o spring rastrei os componentes
 public class GerenciadorCardapioSpringApplication {
 
 	public static void main(String[] args) {
